8cd4cbe6a472db39f20cc629fac53b896c1777b9
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2022-08-03 12:02:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my_random_work
+ Random coding I've done just for fun or to help myself.
---
+++ hour_blocks.py
@@ -0,0 +1,23 @@
+# used for Regex that grabs hour block info from .txt
+import re
+
+HOUR_BLOCKS_PATH = 'hour_blocks.txt'
+HOUR_BLOCKS_REGEX = r'([a-zA-Z]\w+)'
+
+def get_file_contents(file_path):
+	# TODO: Validate string
+	f_open = open(file_path,'r')
+	data = f_open.read()
+	f_open.close()
+	return data
+
+def interpolate_file_contents(file_contents):
+	data = re.findall(HOUR_BLOCKS_REGEX, file_contents)
+	return data
+
+def convertIntoSchedule(hour_blocks):
+	print(f'{hour_blocks}')
+
+raw_hours_content = get_file_contents(HOUR_BLOCKS_PATH)
+hours_data = interpolate_file_contents(raw_hours_content)
+convertIntoSchedule(hours_data)
