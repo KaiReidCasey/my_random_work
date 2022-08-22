cf368ca94b3edcb818b2f0d61592b6daf3afd3d4
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2022-08-22 15:29:52 -0400

    Weight Tracker - Started accessing the weight entries

--- weight_tracker.py
+++ weight_tracker.py
@@ -12,6 +12,7 @@ It is built from a text file
 
 # line = input('> ')
 # print(line)
+ENTRIES_FILE_PATH = 'weight_entries.txt'
 
 def display_options():
 	print('What would you like to do?')
@@ -23,12 +24,22 @@ def get_user_selection():
 	print('You selected ',user_selection)
 	return user_selection
 
+def get_file_contents():
+	# TODO: Validate string
+	f_open = open(ENTRIES_FILE_PATH,'r')
+	data = f_open.read()
+	f_open.close()
+	# temporary to ensure I'm reaching the data
+	print(data) # works! Now to make it pretty
+	# return numbers
+
 def interpret_user_selection(user_selection):
 	if(user_selection == 0):
 		print('Goodbye!')
 		exit()
 	elif(user_selection == 1):
 		print('Entries placeholder')
+		get_file_contents()
 	else:
 		print('Invalid entry. Please try again.')
 		display_options()
