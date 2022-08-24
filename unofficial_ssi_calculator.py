3dcc189b3ee2950f60fa1bc6164cb1a39367c196
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2022-08-24 15:01:24 -0400

    Create unofficial_ssi_calculator.py

---
+++ unofficial_ssi_calculator.py
@@ -0,0 +1,13 @@
+'''
+1. Announce reference page, state, year
+
+2. Get total monthly SSI amount
+
+3. Get earned income, like from a job
+(TODO: Is it before or after taxes? (gross, etc))
+
+4. Calculate
+
+5. Print out amount
+'''
+
