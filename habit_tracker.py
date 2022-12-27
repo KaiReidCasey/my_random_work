a1490714cddf67798aa67602cd9b6c1f48cc1dc6
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2022-12-27 14:06:16 -0500

    Habit Tracker - can save and read dates from file
    Added basic user selection options as well. Needs cleanup - for example the timestamp saved isn't the one displayed, and user should be able to select to read times.

--- habit_tracker.py
+++ habit_tracker.py
@@ -31,8 +31,9 @@ Basically, users should be able to determine whether they are doing more, less, 
 
 TODO:
 + get a timestamp working in Python (print out a time)
-- programmatically create a new file and add timestamp to it
-- append or prepend a timestamp to the existing file
++ programmatically create a new file and add timestamp to it
++ append or prepend a timestamp to the existing file
+- Clean up user selection options
 - recall list of timestamps in human-readable form
 - get a super basic UI going on Windows desktop/laptop
 - add a button
@@ -54,6 +55,8 @@ object representing the current time in UTC is by calling
 datetime.now(timezone.utc)."
 '''
 
+SAVED_TIME_ENTRIES_PATH = 'habit_tracker_time_entries.txt'
+
 '''
 Returns current time
 TODO: verify works for user's time zone, even when they travel 
@@ -61,14 +64,51 @@ TODO: verify works for user's time zone, even when they travel
 def get_current_time():
 	return datetime.now()# Attributes: year, month, day, hour, minute, second, microsecond, and tzinfo.
 
+'''
+Expects file path to .txt file, existing or not
+'''
+def save_current_time(file_path):
+	print('We\'re under construction!')
+	print('Can\'t save current time yet!')
+	time_entry_file = open(file_path,'a')
+	time_entry_file.write(str(get_current_time()))
+	time_entry_file.write('\n')
+	print('Time successfully saved!')
+	time_entry_file.close()
+
+def read_saved_times(file_path):
+	print('Saved times include:')
+	time_entry_file = open(file_path,'r')
+	print(time_entry_file.read())
+	time_entry_file.close()
+
 def greet_user():
 	print('Hello! Welcome to my habit tracker.')
 
-def ask_user_if_save_current_time():
+def display_time_for_user():
 	print(' The current time is: ')
 	print(get_current_time())
+
+def ask_user_if_save_current_time():
+	display_time_for_user()
 	print('Would you like to save it?')
-	print('Sorry, we\'re under construction.')
+	# get user input with error handling
+	try:
+		option_selected = str(input('Enter yes, no, or exit: \n> ').lower())
+		# react to user input
+		if(option_selected == 'y' or option_selected == 'yes'):
+			save_current_time(SAVED_TIME_ENTRIES_PATH)
+			read_saved_times(SAVED_TIME_ENTRIES_PATH)
+		elif(option_selected == 'n' or option_selected == 'no'):
+			# ask again for now since there aren't other options
+			print('We don\'t have other options yet...')
+			ask_user_if_save_current_time()
+		elif(option_selected == 'e' or option_selected == 'exit'):
+			close_habit_tracker()
+		else:
+			print('Incorrect selection. Please enter either yes or no.')
+	finally:
+		print('Thank you!')
 
 def close_habit_tracker():
 	print('Bye!')
